1faad003000fa1c9ca4a1926a25a01047bf2b243
DavidPeterRoberts78 <35351384+DavidPeterRoberts78@users.noreply.github.com>
2021-02-21 16:55:28 +0000

    Update rules.mk

--- keyboards/crkbd/rules.mk
+++ keyboards/crkbd/rules.mk
@@ -26,8 +26,8 @@ MIDI_ENABLE = no            # MIDI controls
 AUDIO_ENABLE = no           # Audio output on port C6
 UNICODE_ENABLE = no         # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
-RGBLIGHT_ENABLE = no       # Enable WS2812 RGB underlight.
-RGB_MATRIX_ENABLE = no
+RGBLIGHT_ENABLE = yes       # Enable WS2812 RGB underlight.
+RGB_MATRIX_ENABLE = yes
 RGB_MATRIX_DRIVER = WS2812
 
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
